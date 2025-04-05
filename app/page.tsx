036bedb80f53d19822e07b45c820af702c625013
shigurenimo <shigurenimo@gmail.com>
2025-04-06 05:19:09 +0900

    update: ライブラリを追加する

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
-  return (
-    <Button>{"Button"}</Button>
-  );
+  return <Button>{"Button"}</Button>
 }
